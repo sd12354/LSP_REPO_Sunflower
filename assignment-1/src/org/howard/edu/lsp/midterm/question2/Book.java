1bda6ea33e251cc524afb1a9acef407448556a18
Sameer Dhanda <78817767+sd12354@users.noreply.github.com>
2025-03-20 19:32:47 -0400

    second question

--- assignment-1/src/org/howard/edu/lsp/midterm/question2/Book.java
+++ assignment-1/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -1,5 +1,82 @@
 package org.howard.edu.lsp.midterm.question2;
 
 public class Book {
+    private String title;
+    private String ISBN;
+    private String author;
+    private int yearPublished;
 
+    /**
+     * Initializes a Book object with the given title, author, ISBN, and year of publication.
+     * 
+     * @param title The title of the book.
+     * @param author The author of the book.
+     * @param ISBN Is the identifier (ISBN) of the book.
+     * @param yearPublished is The year the book was published.
+     */
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.ISBN = ISBN;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return this.ISBN.equals(book.ISBN) && this.author.equals(book.author);
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+}
+
+public class BookDriver {
+    public static void main(String[] args) {
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
+
+        System.out.println(book1.equals(book2)); // true
+        System.out.println(book1.equals(book3)); // false
+        System.out.println(book1); // Output: Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456789, Year Published: 1925
+    }
 }
